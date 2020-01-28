c1769a6b4267d87e2e22797d7c1a3d217d5450be
Rizal Muhamad H <rizal.muhamad@ayomedia.com>
2020-01-28 18:51:33 +0700

    update dependence

--- src/components/Expense-form.svelte
+++ src/components/Expense-form.svelte
@@ -209,7 +209,7 @@
       </div>
     </div>
   </div>
-  <div class="flex items-center justify-between">
+  <div class="flex items-center justify-between mb-4">
     <button
       class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded
       focus:outline-none focus:shadow-outline"
